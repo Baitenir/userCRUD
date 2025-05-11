68d0eb8ea1d52ca94d3128612bf56559de9c1c71
Baitenir <bakumake77@gmail.com>
2025-05-11 14:45:47 +0600

    v-02

--- src/models/User.java
+++ src/models/User.java
@@ -1,4 +1,51 @@
 package models;
+import enums.Gender;
 
 public class User {
+    private Long id;
+    private String name;
+    private int age;
+    private Gender gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
 }
